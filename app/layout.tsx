001ecd311516d2a3356f9eed783eb3aef2079cb5
TechDr <info@techdr.in>
2026-02-10 19:36:57 +0530

    gtm 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11117817231"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-11117817231');`,
+          }}
+        />
+        {/* End Google tag (gtag.js) */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
